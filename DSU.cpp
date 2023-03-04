ef2b805468bb4d86983428af9deb58fcffd54967
Divyansh Sahu <work.divyansh.sahu@gmail.com>
2023-03-05 01:52:21 +0530

    Template class for DSU

---
+++ DSU.cpp
@@ -0,0 +1,46 @@
+class dsu{
+    int *size, *parent, n;
+ 
+public:
+    dsu(int n)
+    {
+        size = new int[n];
+        parent = new int[n];
+        this->n = n;
+        makeSet();
+    }
+    void makeSet()
+    {
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+    int find(int x)
+    {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+ 
+    void unite(int x, int y)
+    {
+	//Unites two sets and makes x the parent of y.
+        int xset = find(x);
+        int yset = find(y);
+ 
+        if (xset == yset){
+            return;
+        }
+ 
+        if(size[xset]<size[yset])swap(xset,yset);
+        parent[yset] = xset;
+        size[xset]+=size[yset];
+    }
+};
+int main(){
+	int ;
+	cin>>n;
+	dsu obj(n);
+}
